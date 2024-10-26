22cb599052839206035615507205b92badc421a8
JLRosales16 <jlrosales@willamette.edu>
2024-10-26 16:51:54 -0700

    Add files via upload

--- Prob1.py
+++ Prob1.py
@@ -2,22 +2,43 @@ from pgl import *
 
 #1a
 def create_histogram_array(data:list[int])->list[int]:
-    pass
+	max_value = max(data) if data else 0
+	histogram = [0] * (max_value + 1)
+	
+	for num in data:
+		histogram[num] += 1
+    
+	return histogram
+    
 
 #1b
-def print_histogram(hist:list[int]) -> None:
-    pass
+def print_histogram(hist: list[int]) -> None:
+    for index, count in enumerate(hist):
+        print(f"{index}: {'*' * count}")
+
 
 #1c
-def graph_histogram(hist:list[int], width:int, height:int) -> None:
+def graph_histogram(hist: list[int], width: int, height: int) -> None:
+    gw = GWindow(width, height)
+    
+    max_count = max(hist) if hist else 1  
 
-    def my_rect(x,y,w,h,color):
-        rect = GRect(x,y,w,h)
-        rect.set_filled(True)
-        rect.set_color(color)
-        gw.add(rect)
+    bar_width = width / len(hist)
+    
+    for i, count in enumerate(hist):
+        bar_height = (count / max_count) * (height - 20) 
 
-    pass
+        x = i * bar_width
+        y = height - bar_height
+        
+        bar = GRect(bar_width - 2, bar_height)  
+        bar.set_filled(True)
+        bar.set_color("red")
+        
+        gw.add(bar, x, y)
+    
+    gw.mainloop()
+
 
 # Some testing printouts for your use!
 PI_DIGITS = [3,1,4,1,5,9,2,6,5,3,5,5,8,9,7,9]
@@ -26,3 +47,5 @@ print(hist)
 #print_histogram(hist) # uncomment to test
 #graph_histogram(hist, 400, 400) # uncomment to test
 
+print_histogram(hist)
+graph_histogram(hist, 400, 400)
